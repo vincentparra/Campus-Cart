5872f1bd163e756bce403624b399fa5573eadb0c
vincentparra <vincent.jovan.resurreccion.parra@gmail.com>
2025-06-02 21:40:13 +0800

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite(['resources/css/homepage.css'])
+  <title>CamputCart</title>
+</head>
+<body>
+  <div class="banner">
+    <h3>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</h3>
+  </div>
+  <div class="desktop_header">
+    <div class="logo"><p>CampusCart</p></div>
+    <div class="nav_links">
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">CONTACT</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">SIGN UP</a></li>
+      </ul>
+    </div>
+    <div class="search">
+      <input type="text" name="search" id="search" placeholder="what are you looking for?">
+    </div>
+  </div>
+
+   <main class="homepage_main">
+    <section class="hero">
+      <div class="hero-text">
+        <h1>Buy and Sell Items with Your Schoolmates</h1>
+        <p>Browse thousands of listings from fellow students. Affordable, convenient, and campus-based.</p>
+        <a href="#" class="btn">Shop Now</a>
+      </div>
+    </section>
+  </main>
+
+</body>
+</html>
